0a4cff34d3185b0dc4fb4947362e3e40f7bbdc0e
Daigon28 <kristhianq28@gmail.com>
2026-09-16 14:06:04 -0500

    migracion para tabla eventos

--- database/migrations/2026_09_16_190553_create_events_table.php
+++ database/migrations/2026_09_16_190553_create_events_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('events', function (Blueprint $table) {
- 
+            $table->id();
+            $table->timestamps();
         });
     }
 
